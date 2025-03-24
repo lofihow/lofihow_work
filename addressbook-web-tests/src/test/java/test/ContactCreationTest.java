3c81da622deba5ba46c5b761a56053067d478ba5
lofihow <katlr5105@gmail.com>
2025-03-24 20:02:07 +0400

    Реализовал параметризированные тесты для создания контактов и групп

--- addressbook-web-tests/src/test/java/test/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/test/ContactCreationTest.java
@@ -1,11 +1,32 @@
+import Model.ContactData;
+import Model.GroupData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import test.TestBase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactCreationTest extends TestBase {
 
-  @Test
-  public void CanCreateContact() {
-
-    app.contact().createcontact();
+  public static List<ContactData> contactProvider() {
+    var result = new ArrayList<ContactData>(List.of(
+            new ContactData("First_name", "Middle_name", "Last_name", "Nickname", "address", "Telephone", "email")));
+    for (int i = 0; i < 7; i++) {
+      result.add(new ContactData(randomString(i * 2), randomString(i * 2), randomString(i * 2), randomString(i * 2), randomString(i * 2), randomString(i * 2), randomString(i * 2)));
+    }
+    return result;
   }
-}
+
+
+    @ParameterizedTest
+    @MethodSource("contactProvider")
+    public void canCreateMultipleContacts(ContactData contact) {
+      int contactCount = app.contact().getCount();
+      app.contact().createcontact(contact);
+      int newContactCount = app.contact().getCount();
+      Assertions.assertEquals(contactCount + 1, newContactCount);
+    }
+  }
